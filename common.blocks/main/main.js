81b947a7140db1c0262ff401bfe636d5da79e549
Vladimir Grinenko <i@tadatuta.com>
2015-05-08 21:13:44 +0300

    next

--- common.blocks/main/main.js
+++ common.blocks/main/main.js
@@ -4,35 +4,23 @@ provide(BEMDOM.decl(this.name, {
     onSetMod: {
         js: {
             inited: function() {
-                var cars = this.params.cars,
-                    modal = this.findBlockInside('modal'),
-                    car = cars[Object.keys(cars)[0]];
+                var modal = this.findBlockInside('modal'),
+                    cars = this.params.cars,
+                    car = this.car = cars[Object.keys(cars)[0]],
+                    model = this.model = this.car[Object.keys(this.car)[0]];
 
                 Select.on(this.elem('car'), 'change', function(e) {
-                    var select = e.target,
-                        val = select.getVal();
-
-                    car = cars[val];
-
-                    if (Objects.isEmpty(car)) {
-                        return BEMDOM.update(this.elem('model'), '');
-                    }
-
-                    BEMDOM.update(this.elem('model'), BEMHTML.apply({
-                        block : 'select',
-                        mods : { mode : 'radio', theme : 'islands', size : 'xl' },
-                        mix : { block: this.block, elem: 'model' },
-                        name : 'model',
-                        val : car[0],
-                        options : Object.keys(car).map(function(model) {
-                            return { val : model, text : model };
-                        })
-                    }));
+                    this.onSelectChange(e, 'model', 'car', cars);
+                    this.onSelectChange(e, 'year', 'model', this.car);
                 }, this);
 
                 Select.on(this.elem('model'), 'change', function(e) {
-                    var model = e.target.getVal(),
-                        colors = car[model].colors;
+                    this.onSelectChange(e, 'year', 'model', this.car);
+                }, this);
+
+                Select.on(this.elem('year'), 'change', function(e) {
+                    var year = e.target.getVal(),
+                        colors = this.model[year].colors;
 
                     modal
                         .setContent('<p>Варианты цветов</p>' +
@@ -43,6 +31,29 @@ provide(BEMDOM.decl(this.name, {
                 }, this);
             }
         }
+    },
+    onSelectChange: function(e, name, field, data) {
+        var select = e.target,
+            val = select.getVal(),
+            option = this[field] = data[val];
+
+        if (Objects.isEmpty(option)) {
+            return BEMDOM.update(this.elem(name), '');
+        }
+
+        BEMDOM.update(this.elem(name), BEMHTML.apply(this.buildSelect(name, option)));
+    },
+    buildSelect: function(name, options) {
+        return {
+            block : 'select',
+            mods : { mode : 'radio', theme : 'islands', size : 'xl' },
+            mix : { block: this.block, elem: name },
+            name : name,
+            val : options[0],
+            options : Object.keys(options).map(function(val) {
+                return { val : val, text : val };
+            })
+        };
     }
 }));
 
